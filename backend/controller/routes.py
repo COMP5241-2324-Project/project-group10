2608b84fb9e8f133ad58b12c9dbde4d313ce8283
Kilingbot <23064755g@connect.polyu.hk>
2024-03-13 16:35:23 +0800

    23064755G Kilingbot - init the structure of backend

--- backend/controller/routes.py
+++ backend/controller/routes.py
@@ -5,12 +5,44 @@ import pymysql
 app = Flask(__name__)
 github_api_url = 'https://api.github.com'
 
+@app.route('/github/repository', methods=['GET'])
+def get_contributors():
+    data = request.get_json()
+    owner = data['data']
+    repo = data['repo']
+    url = github_api_url + '/repos' + '/' + owner + '/' + repo
+    headers = {'Authorization': 'Bearer ' + data['access_token']}
+
+    return 'Success'
+
 @app.route('/github/repository/contributors', methods=['GET'])
 def get_contributors():
     data = request.get_json()
     owner = data['data']
     repo = data['repo']
-    url = github_api_url + '/repos' + '/' + owner + '/' + repo + '/issues'
+    url = github_api_url + '/repos' + '/' + owner + '/' + repo + '/contributors'
+    headers = {'Authorization': 'Bearer ' + data['access_token']}
+
+    return 'Success'
+
+@app.route('/github/repository/pulls', methods=['GET'])
+def get_contributors():
+    data = request.get_json()
+    owner = data['data']
+    repo = data['repo']
+    state = data['state']
+    url = github_api_url + '/repos' + '/' + owner + '/' + repo + '/pulls?' + 'state=' + state
+    headers = {'Authorization': 'Bearer ' + data['access_token']}
+
+    return 'Success'
+
+@app.route('/github/repository/issues', methods=['GET'])
+def get_contributors():
+    data = request.get_json()
+    owner = data['data']
+    repo = data['repo']
+    state = data['state']
+    url = github_api_url + '/repos' + '/' + owner + '/' + repo + '/issues?' + 'state=' + state
     headers = {'Authorization': 'Bearer ' + data['access_token']}
 
     return 'Success'
