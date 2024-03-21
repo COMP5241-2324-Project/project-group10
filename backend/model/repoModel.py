633469fe3efa315313d6621b3fc28d60b1a90506
Kilingbot <23064755g@connect.polyu.hk>
2024-03-21 14:59:25 +0800

    23064755G Kilingbot - db

--- backend/model/repoModel.py
+++ backend/model/repoModel.py
@@ -4,14 +4,26 @@ from backend import db
 class Repo(db.Model):
     """存储库表"""
     __tablename__ = "repo"
+    org_id = db.Column(db.Integer, primary_key=True)
     repo_id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(255))
-    create_user = db.Column(db.String(255))
-    forks = db.Column(db.Integer)
-    issues = db.Column(db.Integer)
-    discription = db.Column(db.String(255))
-    create_time = db.Column(db.DateTime)
-    update_time = db.Column(db.DateTime)
+    repo_name = db.Column(db.String(255))
+    repo_create_time = db.Column(db.DateTime)
+    repo_forks = db.Column(db.Integer)
+    repo_stars = db.Column(db.Integer)
+    repo_watch = db.Column(db.Integer)
+    repo_issues = db.Column(db.Integer)
+    repo_issues_creator = db.Column(db.Integer)
+    repo_issues_open = db.Column(db.Integer)
+    repo_issues_rejected = db.Column(db.Integer)
+    repo_issues_close = db.Column(db.Integer)
+    repo_issues_in_progress = db.Column(db.Integer)
+    repo_pull = db.Column(db.Integer)
+    repo_pull_open = db.Column(db.Integer)
+    repo_pull_merged = db.Column(db.Integer)
+    repo_pull_close = db.Column(db.Integer)
+    repo_commites = db.Column(db.Integer)
+    repo_commiter = db.Column(db.Integer)
+    update_time = db.Column(db.DateTime,primary_key=True)
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
---
+++ backend/model/stuModel.py
@@ -0,0 +1,20 @@
+from backend import db
+
+class Stu(db.Model):
+    """用户表"""
+    __tablename__ = "student_info"
+    user_id = db.Column(db.Integer, primary_key=True)
+    repo_id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(255))
+    user_team_name = db.Column(db.String(255))
+    user_contributions = db.Column(db.Integer)
+    user_commits = db.Column(db.Integer)
+    user_issuses_raised = db.Column(db.Integer)
+    user_pull_requests = db.Column(db.Integer)
+    update_time = db.Column(db.DateTime,primary_key=True)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def __repr__(self):
+        return '<Stu {}>'.format(self.name)
