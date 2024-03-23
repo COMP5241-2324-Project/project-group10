f71746d2a03f4eb564c144ef37032bc034ef4c5e
Yiib0 <2114947467@qq.com>
2024-03-23 19:39:11 +0800

    update userpage

--- frontend/js/userpage.js
+++ frontend/js/userpage.js
@@ -1,40 +1,58 @@
-var user = [{
-    "code": 66,
-    "message": "success",
-    "data": {
-      "user_id": 31,
-      "repo_id": 52,
-      "user_name": "ninowang",
-      "user_team_name": "group_10",
-      "user_contributions": 99,
-      "user_commits": 63,
-      "user_issuses_raised": 30,
-      "user_pull_request": 59,
-      "cur_time": "04-03-31 am 03:50:07"
-    }
-  }]
+var user = {
+  "code": 66,
+  "message": "success",
+  "data": {
+    "user_id": 31,
+    "repo_id": 52,
+    "user_name": "ninowang",
+    "user_team_name": "group_10",
+    "user_contributions": 99,
+    "user_commits": 63,
+    "user_issuses_raised": 30,
+    "user_pull_request": 59,
+    "cur_time": "04-03-31 am 03:50:07"
+  }
+};
+var json4org = {
+  "code": 32,
+  "message": "success",
+  "data": {
+    "org_id": 6,
+    "org_name": "COMP5241-2324-Project",
+    "teacher_name": "cswclui",
+    "repo_count": 23,
+    "org_fork": 77,
+    "org_issues": 63,
+    "org_watch": 6,
+    "org_commites": 94,
+    "org_author_num": 28,
+    "org_commiter_num": 68,
+    "org_pull": 92,
+    "org_pull_open": 69,
+    "org_pull_merged": 40,
+    "org_pull_close": 79,
+    "org_starts": 17,
+    "cur_time": "2024-03-21 11:01:31"
+  }
+};
 
 
-
-var userDetails = user[0].data;
+var userDetails = user.data;
+var orgname = json4org.data.org_name;
 var userName = userDetails.user_name;
 var userId = userDetails.user_id;
-var user_team = userDetails.repo_id;
+var userTeam = userDetails.user_team_name;
+var nameteam = document.getElementById("nameteam");
+nameteam.textContent = " " + userTeam + "/" + userName;
+var icon_name = document.getElementById("icon_name");
+icon_name.textContent = userName;
+var ids = document.getElementById("ids");
+ids.textContent = userId;
+var groups = document.getElementById("groups");
+groups.textContent = userTeam;
 
-var usernameshow = document.getElementById("title_names");
-usernameshow.textContent = userName;
-
-var usernameshow = document.getElementById("names");
-usernameshow.textContent = userName;
-
-var useridshow = document.getElementById("ids");
-useridshow.textContent = userId;
-
-var usergroupshow = document.getElementById("groups")
-usergroupshow.textContent = user_team;
-
-
-
+var repo = document.getElementById("repo");
+repo.textContent = orgname + "/" + userTeam;
 
 var userContributions = userDetails.user_contributions;
 var userCommits = userDetails.user_commits;
@@ -80,110 +98,175 @@ function createStyledCell(width) {
 
 
 var json = [
-    {
-        "code": 89,
-        "message": "success",
-        "data": {
-          "user_id": 62,
-          "activites_date": "1971-08-02 PM 13:52:20",
-          "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
-          "activites_ur": "http://vmcyb.bb/qsrm",
-          "repo_id": 59,
-          "type": "issues",
-          "cur_time": "1985-12-10 AM 09:00:45"
-        }
-    },
-    {
-        "code": 89,
-        "message": "success",
-        "data": {
-          "user_id": 62,
-          "activites_date": "1971-08-02 PM 13:52:20",
-          "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
-          "activites_ur": "http://vmcyb.bb/qsrm",
-          "repo_id": 59,
-          "type": "issues",
-          "cur_time": "1985-12-10 AM 09:00:45"
-        }
-    },
-    {
-        "code": 89,
-        "message": "success",
-        "data": {
-          "user_id": 62,
-          "activites_date": "1971-08-02 PM 13:52:20",
-          "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
-          "activites_ur": "http://vmcyb.bb/qsrm",
-          "repo_id": 59,
-          "type": "issues",
-          "cur_time": "1985-12-10 AM 09:00:45"
-        }
-    },
-    {
-        "code": 89,
-        "message": "success",
-        "data": {
-          "user_id": 62,
-          "activites_date": "1971-08-02 PM 13:52:20",
-          "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
-          "activites_ur": "http://vmcyb.eewerwgvsvgsfbb/qsrm",
-          "repo_id": 59,
-          "type": "issues",
-          "cur_time": "1985-12-10 AM 09:00:45"
-        }
-    },
-    {
-        "code": 89,
-        "message": "success",
-        "data": {
-          "user_id": 62,
-          "activites_date": "1971-08-02 PM 13:52:20",
-          "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
-          "activites_ur": "http://vmcyb.bb/qsrm",
-          "repo_id": 59,
-          "type": "commit",
-          "cur_time": "1985-12-10 AM 09:00:45"
-        }
-    },
-    {
-        "code": 89,
-        "message": "success",
-        "data": {
-          "user_id": 62,
-          "activites_date": "1971-08-02 PM 13:52:20",
-          "activites_message": "Nxubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
-          "activites_ur": "http://vmcbbbbbbbbbbyb.bb/qsrm",
-          "repo_id": 59,
-          "type": "pull requests",
-          "cur_time": "1985-12-10 AM 09:00:45"
-        }
-    },
-    {
-        "code": 89,
-        "message": "success",
-        "data": {
-          "user_id": 62,
-          "activites_date": "1971-08-02 PM 13:52:20",
-          "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
-          "activites_ur": "http://vbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbmcyb.bb/qsrm",
-          "repo_id": 59,
-          "type": "commit",
-          "cur_time": "1985-12-10 AM 09:00:45"
-        }
-    },
-    {
-        "code": 89,
-        "message": "success",
-        "data": {
-          "user_id": 62,
-          "activites_date": "1971-08-02 PM 13:52:20",
-          "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
-          "activites_ur": "http://vmeeeeecyb.bb/qsrm",
-          "repo_id": 59,
-          "type": "issues",
-          "cur_time": "1985-12-10 AM 09:00:45"
-        }
+  {
+    "code": 89,
+    "message": "success",
+    "data": {
+      "user_id": 62,
+      "activites_date": "1971-08-02 PM 13:52:20",
+      "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
+      "activites_ur": "http://vmcyb.bb/qsrm",
+      "repo_id": 59,
+      "type": "issues",
+      "cur_time": "1985-12-10 AM 09:00:45"
     }
+  },
+  {
+    "code": 89,
+    "message": "success",
+    "data": {
+      "user_id": 62,
+      "activites_date": "1971-08-02 PM 13:52:20",
+      "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
+      "activites_ur": "http://vmcyb.bb/qsrm",
+      "repo_id": 59,
+      "type": "issues",
+      "cur_time": "1985-12-10 AM 09:00:45"
+    }
+  },
+  {
+    "code": 89,
+    "message": "success",
+    "data": {
+      "user_id": 62,
+      "activites_date": "1971-08-02 PM 13:52:20",
+      "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
+      "activites_ur": "http://vmcyb.bb/qsrm",
+      "repo_id": 59,
+      "type": "issues",
+      "cur_time": "1985-12-10 AM 09:00:45"
+    }
+  },
+  {
+    "code": 89,
+    "message": "success",
+    "data": {
+      "user_id": 62,
+      "activites_date": "1971-08-02 PM 13:52:20",
+      "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
+      "activites_ur": "http://vmcyb.bb/qsrm",
+      "repo_id": 59,
+      "type": "issues",
+      "cur_time": "1985-12-10 AM 09:00:45"
+    }
+  },
+  {
+    "code": 89,
+    "message": "success",
+    "data": {
+      "user_id": 62,
+      "activites_date": "1971-08-02 PM 13:52:20",
+      "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
+      "activites_ur": "http://vmcyb.bb/qsrm",
+      "repo_id": 59,
+      "type": "issues",
+      "cur_time": "1985-12-10 AM 09:00:45"
+    }
+  },
+  {
+    "code": 89,
+    "message": "success",
+    "data": {
+      "user_id": 62,
+      "activites_date": "1971-08-02 PM 13:52:20",
+      "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
+      "activites_ur": "http://vmcyb.bb/qsrm",
+      "repo_id": 59,
+      "type": "issues",
+      "cur_time": "1985-12-10 AM 09:00:45"
+    }
+  },
+  {
+    "code": 89,
+    "message": "success",
+    "data": {
+      "user_id": 62,
+      "activites_date": "1971-08-02 PM 13:52:20",
+      "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
+      "activites_ur": "http://vmcyb.bb/qsrm",
+      "repo_id": 59,
+      "type": "issues",
+      "cur_time": "1985-12-10 AM 09:00:45"
+    }
+  },
+  {
+    "code": 89,
+    "message": "success",
+    "data": {
+      "user_id": 62,
+      "activites_date": "1971-08-02 PM 13:52:20",
+      "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
+      "activites_ur": "http://vmcyb.bb/qsrm",
+      "repo_id": 59,
+      "type": "issues",
+      "cur_time": "1985-12-10 AM 09:00:45"
+    }
+  },
+  {
+    "code": 89,
+    "message": "success",
+    "data": {
+      "user_id": 62,
+      "activites_date": "1971-08-02 PM 13:52:20",
+      "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
+      "activites_ur": "http://vmcyb.eewerwgvsvgsfbb/qsrm",
+      "repo_id": 59,
+      "type": "issues",
+      "cur_time": "1985-12-10 AM 09:00:45"
+    }
+  },
+  {
+    "code": 89,
+    "message": "success",
+    "data": {
+      "user_id": 62,
+      "activites_date": "1971-08-02 PM 13:52:20",
+      "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
+      "activites_ur": "http://vmcyb.bb/qsrm",
+      "repo_id": 59,
+      "type": "commit",
+      "cur_time": "1985-12-10 AM 09:00:45"
+    }
+  },
+  {
+    "code": 89,
+    "message": "success",
+    "data": {
+      "user_id": 62,
+      "activites_date": "1971-08-02 PM 13:52:20",
+      "activites_message": "Nxubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
+      "activites_ur": "http://vmcbbbbbbbbbbyb.bb/qsrm",
+      "repo_id": 59,
+      "type": "pull requests",
+      "cur_time": "1985-12-10 AM 09:00:45"
+    }
+  },
+  {
+    "code": 89,
+    "message": "success",
+    "data": {
+      "user_id": 62,
+      "activites_date": "1971-08-02 PM 13:52:20",
+      "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
+      "activites_ur": "http://vbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbmcyb.bb/qsrm",
+      "repo_id": 59,
+      "type": "commit",
+      "cur_time": "1985-12-10 AM 09:00:45"
+    }
+  },
+  {
+    "code": 89,
+    "message": "success",
+    "data": {
+      "user_id": 62,
+      "activites_date": "1971-08-02 PM 13:52:20",
+      "activites_message": "Nx  ubpjzyu flf vkexycdd rnkrfzw znm wptjxln wrtccdp hpom fovfiwho vfxpevj.",
+      "activites_ur": "http://vmeeeeecyb.bb/qsrm",
+      "repo_id": 59,
+      "type": "issues",
+      "cur_time": "1985-12-10 AM 09:00:45"
+    }
+  }
 ];
 
 
@@ -252,28 +335,28 @@ table.appendChild(headerRow);
 
 
 for (var i = 0; i < json.length; i++) {
-    var rowData = json[i].data;
+  var rowData = json[i].data;
 
-    if (rowData.type == "commit") {
-        var dataRow = document.createElement("tr");
+  if (rowData.type == "commit") {
+    var dataRow = document.createElement("tr");
 
-        var dataCell1 = createStyledCell('20%');
-        dataCell1.textContent = rowData.activites_date;
+    var dataCell1 = createStyledCell('20%');
+    dataCell1.textContent = rowData.activites_date;
 
-        var dataCell2 = createStyledCell('20%');
-        var link = document.createElement("a");
-        link.href = rowData.activites_ur;
-        link.textContent = rowData.activites_ur;
-        dataCell2.appendChild(link);
+    var dataCell2 = createStyledCell('20%');
+    var link = document.createElement("a");
+    link.href = rowData.activites_ur;
+    link.textContent = rowData.activites_ur;
+    dataCell2.appendChild(link);
 
-        var dataCell3 = createStyledCell('60%');
-        dataCell3.textContent = rowData.activites_message;
+    var dataCell3 = createStyledCell('60%');
+    dataCell3.textContent = rowData.activites_message;
 
-        dataRow.appendChild(dataCell1);
-        dataRow.appendChild(dataCell2);
-        dataRow.appendChild(dataCell3);
-        table.appendChild(dataRow);
-    }
+    dataRow.appendChild(dataCell1);
+    dataRow.appendChild(dataCell2);
+    dataRow.appendChild(dataCell3);
+    table.appendChild(dataRow);
+  }
 }
 
 commitTable.appendChild(table);
@@ -300,28 +383,28 @@ headerRow.appendChild(headerCell3);
 table.appendChild(headerRow);
 
 for (var i = 0; i < json.length; i++) {
-    var rowData = json[i].data;
+  var rowData = json[i].data;
 
-    if (rowData.type == "pull requests") {
-        var dataRow = document.createElement("tr");
+  if (rowData.type == "pull requests") {
+    var dataRow = document.createElement("tr");
 
-        var dataCell1 = createStyledCell('20%');
-        dataCell1.textContent = rowData.activites_date;
+    var dataCell1 = createStyledCell('20%');
+    dataCell1.textContent = rowData.activites_date;
 
-        var dataCell2 = createStyledCell('20%');
-        var link = document.createElement("a");
-        link.href = rowData.activites_ur;
-        link.textContent = rowData.activites_ur;
-        dataCell2.appendChild(link);
+    var dataCell2 = createStyledCell('20%');
+    var link = document.createElement("a");
+    link.href = rowData.activites_ur;
+    link.textContent = rowData.activites_ur;
+    dataCell2.appendChild(link);
 
-        var dataCell3 = createStyledCell('60%');
-        dataCell3.textContent = rowData.activites_message;
+    var dataCell3 = createStyledCell('60%');
+    dataCell3.textContent = rowData.activites_message;
 
-        dataRow.appendChild(dataCell1);
-        dataRow.appendChild(dataCell2);
-        dataRow.appendChild(dataCell3);
-        table.appendChild(dataRow);
-    }
+    dataRow.appendChild(dataCell1);
+    dataRow.appendChild(dataCell2);
+    dataRow.appendChild(dataCell3);
+    table.appendChild(dataRow);
+  }
 }
 
 pullTable.appendChild(table);
@@ -343,24 +426,24 @@ commitTable.style.display = "none";
 pullTable.style.display = "none";
 
 
-commitButton.addEventListener("click", function() {
-    issueTable.style.display = "none";
-    commitTable.style.display = "table";
-    pullTable.style.display = "none";
+commitButton.addEventListener("click", function () {
+  issueTable.style.display = "none";
+  commitTable.style.display = "table";
+  pullTable.style.display = "none";
 });
 
 
-pullButton.addEventListener("click", function() {
-    issueTable.style.display = "none";
-    commitTable.style.display = "none";
-    pullTable.style.display = "table";
+pullButton.addEventListener("click", function () {
+  issueTable.style.display = "none";
+  commitTable.style.display = "none";
+  pullTable.style.display = "table";
 });
 
 
-issueButton.addEventListener("click", function() {
-    issueTable.style.display = "table";
-    commitTable.style.display = "none";
-    pullTable.style.display = "none";
+issueButton.addEventListener("click", function () {
+  issueTable.style.display = "table";
+  commitTable.style.display = "none";
+  pullTable.style.display = "none";
 });
 
 
